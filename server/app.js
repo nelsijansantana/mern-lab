01db04b7c3f845a276cd7a3d301ccd66c1341070
Elisaudo <elisaudos@gmail.com>
2025-07-03 17:25:22 -0300

    primeiro commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const connectDB = require('./config/db');
+
+const booksRoutes = require('./routes/api/books');
+
+const cors = require('cors');
+
+const app = express();
+
+//Middleware
+app.use(express.json({extended:false}));
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// Conecta o  banco de dados
+connectDB();
+
+//Cors
+app.use(cors({origin: true, credentials: true}));
+
+//Routes
+app.use('/api/books', booksRoutes);
+
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port} `));
